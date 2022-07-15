08538bf90b6a27b35d8109a22e22de4dcd8c4d84
Harshad2210 <harshadbanate.2210@gmail.com>
2022-07-15 13:13:13 +0530

    Added authorisation in all routes

--- routes/stackoverflowRoutes.js
+++ routes/stackoverflowRoutes.js
@@ -6,25 +6,23 @@ const router = express.Router();
 
 router.post("/register", stackoverflowController.registerUser);
 
-router.post("/users/:uid/block", stackoverflowController.blockUser);
+router.post("/users/:uid/block", verifyToken, stackoverflowController.blockUser);
 
-router.post("/users/:uid/block", stackoverflowController.unblockUser);
+router.post("/users/:uid/unblock",verifyToken, stackoverflowController.unblockUser);
 
 router.post("/questions", verifyToken, stackoverflowController.addQuestion)
 
 router.get("/questions", stackoverflowController.getQuestions)
 
-router.get("/questions", stackoverflowController.getQuestions)
-
 router.get("/questions/:qid/block", stackoverflowController.blockQuestion)
 
-router.get("/questions/:qid/unblock", stackoverflowController.unblockQuestion)
+router.get("/questions/:qid/unblock", verifyToken, stackoverflowController.unblockQuestion)
 
-router.post("/questions/:qid/answers", stackoverflowController.addAnwers)
+router.post("/questions/:qid/answers",verifyToken, stackoverflowController.addAnwers)
 
-router.post("/questions/:qid/answers/block", stackoverflowController.blockAnswer)
+router.post("/questions/:qid/answers/block",verifyToken, stackoverflowController.blockAnswer)
 
-router.post("/questions/:qid/answers/unblock", stackoverflowController.unblockAnswer)
+router.post("/questions/:qid/answers/unblock",verifyToken, stackoverflowController.unblockAnswer)
 
 
 module.exports = router;
